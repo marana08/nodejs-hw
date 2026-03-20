b701f866a3d69c9c249a6f4cd06cefd4b5029ce8
mariana <maranamelen13@gmail.com>
2026-03-20 17:50:03 +0200

    mongodb

---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,13 @@
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    return res.status(err.statusCode).json({
+      message: err.message || err.name,
+    });
+  }
+  const isProd = process.env.NODE_ENV;
+  res.status(500).json({
+    message: isProd ? 'Something went wrong. Please try again later.'  : err.stack,
+  });
+};
