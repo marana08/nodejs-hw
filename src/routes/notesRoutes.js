2d45aa77b2f75f82f76eb8fc9c8b1e217c5cbdf6
mariana <maranamelen13@gmail.com>
2026-03-30 16:38:04 +0300

    hw

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,18 +1,47 @@
 import { Router } from 'express';
+import { celebrate } from 'celebrate';
+
 import {
   createNote,
-	deleteNote,
-	getAllNotes,
-	getNoteById,
-  updateNote
+  deleteNote,
+  getAllNotes,
+  getNoteById,
+  updateNote,
 } from '../controllers/notesController.js';
+
+import {
+  getAllNotesSchema,
+  noteIdSchema,
+  createNoteSchema,
+  updateNoteSchema,
+} from '../validations/notesValidaton.js';
 
 const router = Router();
 
-router.get('/notes', getAllNotes);
-router.get('/notes/:noteId', getNoteById);
-router.post('/notes', createNote);
-router.delete("/notes/:noteId", deleteNote);
-router.patch('/notes/:noteId', updateNote);
+router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
+
+router.get(
+  '/notes/:noteId',
+  celebrate(noteIdSchema),
+  getNoteById
+);
+
+router.post(
+  '/notes',
+  celebrate(createNoteSchema),
+  createNote
+);
+
+router.delete(
+  '/notes/:noteId',
+  celebrate(noteIdSchema),
+  deleteNote
+);
+
+router.patch(
+  '/notes/:noteId',
+  celebrate(updateNoteSchema),
+  updateNote
+);
 
 export default router;
